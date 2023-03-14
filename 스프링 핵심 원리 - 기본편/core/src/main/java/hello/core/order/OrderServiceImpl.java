a902f4e120b71ab1360aed249ed01e5d57cb3a36
Taeho <ice_98@naver.com>
2023-03-14 23:56:13 +0900

    #1 - 기존의 DIP 문제가있던 코드 수정

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,15 +10,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy; //이것만 하면 nullpointerror가 나옴.. 어케 해결하지? > AppConfig 생성!!!!!!
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //인터페이스,구현체 둘다 의존 -> DIP 위반 > 인터페이스에만 의존하도록 변경.
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 둘중에 하나로 변경할때 코드를 변경해야 하므로, OCP 위반이다.
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
