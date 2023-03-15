81a5add95b58a5d5f1eecfcf6eb6a2eaa88ad552
Taeho <ice_98@naver.com>
2023-03-15 23:09:13 +0900

    #1 -  중복제거, 한눈에 구조가 들어오기 위해 리팩토링

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
@@ -11,10 +12,17 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
