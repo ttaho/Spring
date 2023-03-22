4a878fdcde3d55d4cae36a07aea951c8cfc8744c
Taeho <ice_98@naver.com>
2023-03-22 23:50:04 +0900

    #3 - @Configuration과 싱글톤

---
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,29 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = orderService.getMemberRepository();
+        MemberRepository memberRepository2 = memberService.getMemberRepository();
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
+        System.out.println("orderService -> memberRepository1 = " + memberRepository1);
+        System.out.println("memberService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository -> memberRepository2 = " + memberRepository);
+    }
+}
