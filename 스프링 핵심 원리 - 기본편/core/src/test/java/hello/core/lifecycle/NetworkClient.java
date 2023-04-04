27272cdb756ba8d891a0d3f56a8525fe1e9450a4
Taeho <ice_98@naver.com>
2023-04-04 21:55:20 +0900

    #6 - 빈 생명주기 콜백

--- 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,17 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 public class NetworkClient {
 
     private String url;
 
+
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-
     }
 
     public void setUrl(String url) {
@@ -21,11 +24,28 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call = " + url + "message = " + message);
+        System.out.println("call = " + url + " message = " + message);
     }
 
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+
+
+
+    //스프링 의존관계 주입이 끝나고 그 이후에 호출
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
